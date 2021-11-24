c7d878c414df887874bc739d09a3390e47ccef44
doyledyp <doyledev@protonmail.com>
2021-11-24 13:19:23 +0000

    Remove other News old iDYP Allocation

--- src/components/carouselNews.js
+++ src/components/carouselNews.js
@@ -51,16 +51,16 @@ export default class Carousel extends React.Component {
                         <NavLink to="/farmv2">Go to Farms and Start Earning Rewards</NavLink>
 
                     </div>
-                    <div className="">
-                        <h3>iDYP x DeFi Yield Protocol
-                        </h3>
-                        <p>iDYP is an excellent investment for DYP holders who wish to get a passive income by using our
-                            products. Our new smart contracts will use iDYP as part of novel strategies, adding constant
-                            buying pressure on the DYP token and boost the rewards for the farm, buyback, and staking
-                            pools.
-                        </p>
-                        <NavLink to="/idyp">Go to iDYP Community Allocation</NavLink>
-                    </div>
+                    {/*<div className="">*/}
+                    {/*    <h3>iDYP x DeFi Yield Protocol*/}
+                    {/*    </h3>*/}
+                    {/*    <p>iDYP is an excellent investment for DYP holders who wish to get a passive income by using our*/}
+                    {/*        products. Our new smart contracts will use iDYP as part of novel strategies, adding constant*/}
+                    {/*        buying pressure on the DYP token and boost the rewards for the farm, buyback, and staking*/}
+                    {/*        pools.*/}
+                    {/*    </p>*/}
+                    {/*    <NavLink to="/idyp">Go to iDYP Community Allocation</NavLink>*/}
+                    {/*</div>*/}
                 </OwlCarousel>
                 <a className='sliding-link' href='#brand-area'>
                     <span><img className="logo" src="img/bottom-arrow.svg" alt="images not found" /></span>
