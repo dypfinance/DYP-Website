3529e533a209860a9be489e0029efd9c16097e7f
doyledyp <doyledev@protonmail.com>
2022-01-10 18:33:55 +0000

    Update Home Stats

--- src/components/home.js
+++ src/components/home.js
@@ -222,7 +222,7 @@ export default class Home extends React.Component {
                                 <div className="row" style={{display: 'flex', width: '100%', justifyContent: 'space-between', alignItems: 'center', margin: '0px'}}>
                                     <div className="col-lg-1 col-sm-4 col-6" style={{display: 'grid', justifyContent: 'center', justifyItems: 'center'}}>
                                       <i className="fas fa-money-bill-wave" style={{fontSize: '48px'}}></i>
-                                      <span style={{fontSize: '48px'}}>$487M<span style={{opacity: '0.1', fontSize: '48px'}}>+</span></span>
+                                      <span style={{fontSize: '48px'}}>$775M<span style={{opacity: '0.1', fontSize: '48px'}}>+</span></span>
                                         <p style={{fontSize: '14px', marginTop: '-15px'}}>All Time Volume</p>
                                     </div>
                                     <div className="col-lg-1 col-sm-4 col-6" style={{display: 'grid', justifyItems: 'center'}}><i className="fas fa-hand-holding-usd" style={{fontSize: '48px'}}></i><span style={{fontSize: '48px'}}>$43M<span style={{opacity: '0.1', fontSize: '48px'}}>+</span></span>
@@ -231,7 +231,7 @@ export default class Home extends React.Component {
                                     <div className="col-lg-1 col-sm-4 col-6" style={{display: 'grid', justifyItems: 'center'}}><i className="fas fa-users" style={{fontSize: '48px'}}></i><span style={{fontSize: '48px'}}>2.5K<span style={{opacity: '0.1', fontSize: '48px'}}>+</span></span>
                                         <p style={{fontSize: '14px', marginTop: '-15px', width: '105px', maxWidth: '100%'}}>Liquidity Providers</p>
                                     </div>
-                                    <div className="col-lg-1 col-sm-4 col-6" style={{display: 'grid', justifyItems: 'center'}}><i className="fas fa-coins" style={{fontSize: '48px'}}></i><span style={{fontSize: '48px'}}>59</span>
+                                    <div className="col-lg-1 col-sm-4 col-6" style={{display: 'grid', justifyItems: 'center'}}><i className="fas fa-coins" style={{fontSize: '48px'}}></i><span style={{fontSize: '48px'}}>68</span>
                                         <p style={{fontSize: '14px', marginTop: '-15px', width: '70px', maxWidth: '100%'}}>Active Pools</p>
                                     </div>
                                     <div className="col-lg-1 col-sm-4 col-6" style={{display: 'grid', justifyItems: 'center'}}><i className="fas fa-laptop-code" style={{fontSize: '48px'}}></i><span style={{fontSize: '48px'}}>10</span>
