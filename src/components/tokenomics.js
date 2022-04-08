fbea658d8f3eb4335a0559c276d907c4bd8f9722
LorenaL579 <lorena.lici579@gmail.com>
2022-04-08 14:03:46 +0200

    first ui update in vote page and tokenomics

--- src/components/tokenomics.js
+++ src/components/tokenomics.js
@@ -1,159 +1,233 @@
-import React from 'react'
-import getFormattedNumber from '../functions/get-formatted-number'
+import React from "react";
+import getFormattedNumber from "../functions/get-formatted-number";
 
 export default class Tokenomics extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      circSupply: "",
+    };
+  }
 
-    constructor(props) {
-      super(props)
-      this.state = {
-        circSupply: ''
-      }
-    }
+  componentDidMount() {
+    this.getCirculatingSupply();
+  }
+  getCirculatingSupply = async () => {
+    let circ = 0;
+    circ = await window.getCirculatingSupply();
 
-    componentDidMount() {
-      this.getCirculatingSupply()
-    }
-    getCirculatingSupply = async () => {
-      let circ = 0
-      circ = await window.getCirculatingSupply()
+    let circSupply = circ;
+    this.setState({ circSupply });
+    return circSupply;
+  };
 
-      let circSupply = circ
-      this.setState({circSupply})
-      return circSupply
-    }
-
-
-    render() {
-        return (
-            <div className="p4token-bg App-container">
-    <div className="p4token">
-       <div className="container">
-          <div className="row">
-             <div className="col-lg-10 offset-lg-1">
-                <div className="p4token-wrapper" style={{paddingBottom: '25px'}}>
-                   {/*<div className="p4token-heading">
+  render() {
+    return (
+      <div className="p4token-bg App-container" style={{ paddingTop: 60 }}>
+        <div className="p4token">
+          <div className="container">
+            <h1
+              className="col-lg-10 offset-lg-1 pl-0"
+              style={{ fontWeight: "bold" }}
+            >
+              Tokenomics
+            </h1>
+            <div className="row">
+              <div className="col-lg-10 offset-lg-1">
+                <div>
+                  <div
+                    className="p4token-wrapperTOP"
+                    style={{
+                      background: "none",
+                      boxShadow: "none",
+                      padding: 0,
+                    }}
+                  >
+                    {/*<div className="p4token-heading">
                       <p className='h3'><b>Tokenomics</b></p>
                    </div>*/}
-                   <div className="p4token-content">
-                      <div className="p4token-content-one">
-                         <p>DYP Contract Address: </p>
-                         <p className="p4token-content-strong"><span>0x961C8c0B1aaD0c0b10a51FeF6a867E3091BCef17</span></p>
+                    <div className="col-lg-12 offset-lg-2 ml-0 p-0 d-flex">
+                      <div
+                        className="p4token-content-one col-lg-8 p4token-wrapper"
+                        style={{ display: "grid", padding: 25 }}
+                      >
+                        <p>DYP Contract Address: </p>
+                        <p className="p4token-content-strong">
+                          <span>
+                            0x961C8c0B1aaD0c0b10a51FeF6a867E3091BCef17
+                          </span>
+                        </p>
                       </div>
-                      <div className="p4token-content-one">
-                         <p>Initial Circulating Supply: </p>
-                         <p className="p4token-content-strong"><span>{getFormattedNumber(this.state.circSupply,2)} DYP</span></p>
+                      <div className="supplyCirculated col-lg-4 p-0">
+                        <p style={{ fontSize: 12 }}>
+                          Initial Circulating Supply:{" "}
+                        </p>
+                        <p className="p4token-content-strong">
+                          <span>
+                            {getFormattedNumber(this.state.circSupply, 2)} DYP
+                          </span>
+                        </p>
                       </div>
-                       <div id="crypto-widget-CoinList" data-design="classic" data-coins="defi-yield-protocol"></div>
-                   </div>
-                </div>
-             </div>
-          </div>
-       </div>
-    </div>
-
-    <div className="p4dyp">
-       <div className="container">
-          <div className="row">
-             <div className="col-lg-10 offset-lg-1">
-                <div className="p4dyp-wrapper">
-                   <div className="p4dyp-heading">
-                      <h5>DYP Allocation</h5>
-                      <p>30,000,000 DYP have been minted at genesis and will become accessible over the course of 2 years.  <br />
-                      The 2-year allocation is as follows:</p>
-                   </div>
-                   <div className="p4dyp-content">
-                      <div className="p4dyp-content-one">
-                         <div className="p4dyp-content-one-left">
-                            <p>Community</p>
-                         </div>
-                         <div className="p4dyp-content-one-left">
-                            <h4>12,000,000 DYP</h4>
-                            <p>Distributed for pool rewards over the next 12 months</p>
-                         </div>
-                         <div className="p4dyp-content-one-left">
-                            <h4>4,500,000 DYP</h4>
-                            <p>Distributed to Binance Smart Chain</p>
-                         </div>
-                          <div className="p4dyp-content-one-left">
-                              <h4>2,090,000 DYP</h4>
-                              <p>Distributed to Avalanche Network</p>
-                          </div>
-                          <div className="p4dyp-content-one-left">
-                              <h4>1,214,111 DYP</h4>
-                              <p>Distributed for providing liquidity to Centralized Exchanges and Marketing Purposes</p>
-                          </div>
-                         <div className="p4dyp-content-one-left">
-                            <h4>2,584,689 DYP</h4>
-                            <p>Reserved for other pools or chains - Locked on smart contract</p>
-                         </div>
-                         <div className="p4dyp-content-one-left" style={{borderBottom: 0}}>
-                            <h4>200,000 DYP</h4>
-                            <p>Locked for one year to Uniswap liquidity on token launch</p>
-                         </div>
-                      </div>
-                   </div>
-                   <div className="p4dyp-content">
-                      <div className="p4dyp-content-one">
-                         <div className="p4dyp-content-one-left">
-                            <p>Team</p>
-                         </div>
-                         <div className="p4dyp-content-one-left" style={{borderBottom: 0}}>
-                            <h4>2,411,200 DYP</h4>
-                            <p>Vested for 24 months, released monthly</p>
-                         </div>
-                      </div>
-                   </div>
-                   <div className="p4dyp-content">
-                      <div className="p4dyp-content-one">
-                         <div className="p4dyp-content-one-left">
-                            <p>Investors</p>
-                         </div>
-                         <div className="p4dyp-content-one-left">
-                            <h4>651,531 DYP</h4>
-                            <p>Public Sale</p>
-                         </div>
-                         <div className="p4dyp-content-one-left" style={{borderBottom: 0}}>
-                            <h4>4,348,469 DYP</h4>
-                            <p style={{wordWrap: 'break-word'}}>BURNED & REMOVED from the total supply TH: <br />
-                              <a href="https://etherscan.io/tx/0x69d1faef4d41752c200dfe68b103c2823f94f225c502b455321bae0d94ebf8a5" target="_blank">
-                                0x69d1faef4d41752c200dfe68b103c2823f94f225c502b455321bae0d94ebf8a5
-                              </a>
-                            </p>
-                         </div>
-                      </div>
-                   </div>
-                   <div className="p4dyp-can">
-                      <p id="noadd"><strong>No Additional Tokens Can Be Minted!</strong></p>
-                   </div>
-                </div>
-             </div>
-          </div>
-       </div>
-    </div>
-
-    <div className="p4token-sap">
-       <div className="container">
-          <div className="row">
-             <div className="col-lg-10 offset-lg-1">
+                    </div>
+                    <div className="p4token-content">
+                      <div
+                        id="crypto-widget-CoinList"
+                        data-design="classic"
+                        data-coins="defi-yield-protocol"
+                      ></div>
+                    </div>
+                    
+                  </div>
+                  <div className="p4token-sap">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-10 offset-lg-1">
                 <div className="p4token-sap-wrapper">
-                   <div className="p4token-sap-cotnent">
-                      <ul>
-                         <li><span></span>Community</li>
-                         <li><span></span>Team</li>
-                         <li><span></span>Investors</li>
-                      </ul>
-                   </div>
-                   <div className="p4token-sap-img">
-                      <img id="whitelink" src="/img/tsap.svg" alt="images not found" />
-                      <img id="darklink" src="/img/dstp.svg" alt="images not found" />
-                   </div>
+                  <div className="p4token-sap-cotnent">
+                    <ul>
+                      <li>
+                        <span></span>Community
+                      </li>
+                      <li>
+                        <span></span>Team
+                      </li>
+                      <li>
+                        <span></span>Investors
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="p4token-sap-img">
+                    <img
+                      id="whitelink"
+                      src="/img/tsap.svg"
+                      alt="images not found"
+                    />
+                    <img
+                      id="darklink"
+                      src="/img/dstp.svg"
+                      alt="images not found"
+                    />
+                  </div>
                 </div>
-             </div>
+              </div>
+            </div>
           </div>
-       </div>
-    </div>
+        </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
- </div>
-        )
-    }
+        <div className="p4dyp">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-10 offset-lg-1">
+                <div className="p4dyp-wrapper">
+                  <div className="p4dyp-heading">
+                    <h5>DYP Allocation</h5>
+                    <p>
+                      30,000,000 DYP have been minted at genesis and will become
+                      accessible over the course of 2 years. <br />
+                      The 2-year allocation is as follows:
+                    </p>
+                  </div>
+                  <div className="p4dyp-content">
+                    <div className="p4dyp-content-one">
+                      <div className="p4dyp-content-one-left">
+                        <p>Community</p>
+                      </div>
+                      <div className="p4dyp-content-one-left">
+                        <h4>12,000,000 DYP</h4>
+                        <p>
+                          Distributed for pool rewards over the next 12 months
+                        </p>
+                      </div>
+                      <div className="p4dyp-content-one-left">
+                        <h4>4,500,000 DYP</h4>
+                        <p>Distributed to Binance Smart Chain</p>
+                      </div>
+                      <div className="p4dyp-content-one-left">
+                        <h4>2,090,000 DYP</h4>
+                        <p>Distributed to Avalanche Network</p>
+                      </div>
+                      <div className="p4dyp-content-one-left">
+                        <h4>1,214,111 DYP</h4>
+                        <p>
+                          Distributed for providing liquidity to Centralized
+                          Exchanges and Marketing Purposes
+                        </p>
+                      </div>
+                      <div className="p4dyp-content-one-left">
+                        <h4>2,584,689 DYP</h4>
+                        <p>
+                          Reserved for other pools or chains - Locked on smart
+                          contract
+                        </p>
+                      </div>
+                      <div
+                        className="p4dyp-content-one-left"
+                        style={{ borderBottom: 0 }}
+                      >
+                        <h4>200,000 DYP</h4>
+                        <p>
+                          Locked for one year to Uniswap liquidity on token
+                          launch
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="p4dyp-content">
+                    <div className="p4dyp-content-one">
+                      <div className="p4dyp-content-one-left">
+                        <p>Team</p>
+                      </div>
+                      <div
+                        className="p4dyp-content-one-left"
+                        style={{ borderBottom: 0 }}
+                      >
+                        <h4>2,411,200 DYP</h4>
+                        <p>Vested for 24 months, released monthly</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="p4dyp-content">
+                    <div className="p4dyp-content-one">
+                      <div className="p4dyp-content-one-left">
+                        <p>Investors</p>
+                      </div>
+                      <div className="p4dyp-content-one-left">
+                        <h4>651,531 DYP</h4>
+                        <p>Public Sale</p>
+                      </div>
+                      <div
+                        className="p4dyp-content-one-left"
+                        style={{ borderBottom: 0 }}
+                      >
+                        <h4>4,348,469 DYP</h4>
+                        <p style={{ wordWrap: "break-word" }}>
+                          BURNED & REMOVED from the total supply TH: <br />
+                          <a
+                            href="https://etherscan.io/tx/0x69d1faef4d41752c200dfe68b103c2823f94f225c502b455321bae0d94ebf8a5"
+                            target="_blank"
+                          >
+                            0x69d1faef4d41752c200dfe68b103c2823f94f225c502b455321bae0d94ebf8a5
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="p4dyp-can">
+                    <p id="noadd">
+                      <strong>No Additional Tokens Can Be Minted!</strong>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/vote.js
+++ src/components/vote.js
@@ -1,80 +1,100 @@
-import React from 'react'
+import React from "react";
 
 export default class Vote extends React.Component {
-    render() {
-        return (
-            <>
-                  <div className="earn-hero-area App-container">
-        <div className="container">
-           <div className="earn-hero-wrapper">
+  render() {
+    return (
+      <>
+        <div className="earn-hero-area App-container">
+          <div className="container">
+            <div className="earn-hero-wrapper">
               <div className="row">
-                 <div className="col-lg-8  offset-lg-2">
-                    <div className="earn-hero-content p4token-wrapper">
-                      <div className='text-left'>
-                        <p className='h3'><b>DYP Governance</b></p>
-                        {/* <h5>Visit <a style={{textTransform: 'lowercase'}} href='https://mining.dyp.finance'>https://mining.dyp.finance</a> to join the Ethereum Mining Pool Whitelist</h5> */}
-                        <p>DYP tokens represent voting shares in DeFi Yield Protocol governance. The introduction of DYP tokens enables shared community ownership of a vibrant, diverse, and dedicated governance system which will actively guide the protocol toward the future.</p>
-                        <p>Through governance, DYP holders can vote to add more pools, burn tokens, or allocate DYP toward grants, strategic partnerships, governance initiatives, and other programs.</p>
-                        {/*<div style={{display: "flex"}}>*/}
-                        {/*  <div className="earn-hero-btns-bridge m-0">*/}
-                        {/*    <a target='_blank' href='https://gov.dyp.finance'>ETH Governance</a>*/}
-                        {/*  </div>*/}
-                        {/*  <div className="earn-hero-btns-white m-0 pl-3">*/}
-                        {/*    <a target='_blank' href='https://gov-bsc.dyp.finance'>BSC Governance</a>*/}
-                        {/*  </div>*/}
-                        {/*</div>*/}
-                        <div className="earn-hero-btns">
-                          {/* <a href="https://dypfinance.medium.com/defi-yield-protocol-bridge-tutorial-9dad5226537f">DYP Bridge Tutorial</a> */}
-                          {/* <a href="#">Read More about DYP</a> */}
-                        </div>
+                <div className="col-lg-12  offset-lg-2 ml-0">
+                  <div className="earn-hero-content p4token-wrapper" style={{boxShadow: 'none'}}>
+                    <div className="text-left" style={{borderBottom: '1px solid #999999'}}>
+                      <h1>
+                        <b>DYP Governance</b>
+                      </h1>
+                      {/* <h5>Visit <a style={{textTransform: 'lowercase'}} href='https://mining.dyp.finance'>https://mining.dyp.finance</a> to join the Ethereum Mining Pool Whitelist</h5> */}
+                     <div className="GovDesc">
+                      <p className="col-lg-6 col-md-4 col-12 pl-0">
+                        DYP tokens represent voting shares in DeFi Yield
+                        Protocol governance. The introduction of DYP tokens
+                        enables shared community ownership of a vibrant,
+                        diverse, and dedicated governance system which will
+                        actively guide the protocol toward the future.
+                      </p>
+                      <p className="col-lg-6 col-md-4 col-12 pl-0">
+                        Through governance, DYP holders can vote to add more
+                        pools, burn tokens, or allocate DYP toward grants,
+                        strategic partnerships, governance initiatives, and
+                        other programs.
+                      </p>
+                      </div>
+                      {/*<div style={{display: "flex"}}>*/}
+                      {/*  <div className="earn-hero-btns-bridge m-0">*/}
+                      {/*    <a target='_blank' href='https://gov.dyp.finance'>ETH Governance</a>*/}
+                      {/*  </div>*/}
+                      {/*  <div className="earn-hero-btns-white m-0 pl-3">*/}
+                      {/*    <a target='_blank' href='https://gov-bsc.dyp.finance'>BSC Governance</a>*/}
+                      {/*  </div>*/}
+                      {/*</div>*/}
+                      <div className="earn-hero-btns">
+                        {/* <a href="https://dypfinance.medium.com/defi-yield-protocol-bridge-tutorial-9dad5226537f">DYP Bridge Tutorial</a> */}
+                        {/* <a href="#">Read More about DYP</a> */}
                       </div>
                     </div>
-                 </div>
-                  <div className='col-lg-8 offset-lg-2 mt-5'>
-                      <div className="farming-content">
-                          <div className="row">
-                              <div className="col-lg-4 col-md-4 col-6">
-                                  <a href="https://gov.dyp.finance/" target='_blank'>
-                                      <div className="fariming-item">
-                                          <div className="icon">
-                                              <img src="img/i1.svg" alt="Image not found" />
-                                              {/*<div className="line"></div>*/}
-                                          </div>
-                                          {/*<p>Blockchain Consilium</p>*/}
-                                      </div>
-                                  </a>
-                              </div>
-                              <div className="col-lg-4 col-md-4 col-6">
-                                  <a href='https://gov-bsc.dyp.finance/' target='_blank'>
-                                      <div className="fariming-item">
-                                          <div className="icon">
-                                              <img src="img/farms/bnb-yield.svg" alt="Image not found" />
-                                          </div>
-                                          {/*<div className="line"></div>*/}
-                                          {/*<p>Certik Foundation</p>*/}
-                                      </div>
-                                  </a>
-                              </div>
-                              <div className="col-lg-4 col-md-4 col-6">
-                                  <a href="https://gov-avax.dyp.finance/" target='_blank'>
-                                      <div className="fariming-item">
-                                          <div className="icon">
-                                              <img src="img/farms/avax-yield.png" alt="Image not found" />
-                                          </div>
-                                          {/*<div className="line"></div>*/}
-                                          {/*<p>Peckshield</p>*/}
-                                      </div>
-                                  </a>
-                              </div>
-                          </div>
-                      </div>
                   </div>
+                </div>
+                <div className="col-lg-12 offset-lg-2 mt-5 ml-0">
+                  <div className="farming-content">
+                    <div className="row" style={{justifyContent: 'center'}}>
+                      <div className="">
+                        <a href="https://gov.dyp.finance/" target="_blank">
+                          <div className="fariming-item" style={{boxShadow: 'none', background: 'none'}}>
+                            <div className="icon">
+                              <img src="img/ethBanner.svg" alt="Image not found" />
+                              {/*<div className="line"></div>*/}
+                            </div>
+                            {/*<p>Blockchain Consilium</p>*/}
+                          </div>
+                        </a>
+                      </div>
+                      <div className="">
+                        <a href="https://gov-bsc.dyp.finance/" target="_blank">
+                          <div className="fariming-item" style={{boxShadow: 'none', background: 'none'}}>
+                            <div className="icon">
+                              <img
+                                src="img/bscBanner.svg"
+                                alt="Image not found"
+                              />
+                            </div>
+                            {/*<div className="line"></div>*/}
+                            {/*<p>Certik Foundation</p>*/}
+                          </div>
+                        </a>
+                      </div>
+                      <div className="">
+                        <a href="https://gov-avax.dyp.finance/" target="_blank">
+                          <div className="fariming-item" style={{boxShadow: 'none', background: 'none'}}>
+                            <div className="icon">
+                              <img
+                                src="img/avaxBanner.svg"
+                                alt="Image not found"
+                              />
+                            </div>
+                            {/*<div className="line"></div>*/}
+                            {/*<p>Peckshield</p>*/}
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-           </div>
+            </div>
+          </div>
         </div>
-     </div>
-
-         </>
-        )
-    }
+      </>
+    );
+  }
 }
