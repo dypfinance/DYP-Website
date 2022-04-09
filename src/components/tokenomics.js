c82e79fd47d0a38135dc383fe5f73b71e50db647
ortensa <ortensal77@gmail.com>
2022-04-09 14:53:06 +0200

    wip: new design for tokenomics

--- src/components/tokenomics.js
+++ src/components/tokenomics.js
@@ -1,5 +1,8 @@
 import React from "react";
 import getFormattedNumber from "../functions/get-formatted-number";
+import rightArrow from "../../src/assets/svg/white-arrow.svg";
+import graphic from "../../src/assets/images/graphics.svg";
+import lineGraph from "../../src/assets/images/line-graph.svg";
 
 export default class Tokenomics extends React.Component {
   constructor(props) {
@@ -33,24 +36,19 @@ export default class Tokenomics extends React.Component {
               Tokenomics
             </h1>
             <div className="row">
-              <div className="col-lg-10 offset-lg-1">
-                <div>
-                  <div
-                    className="p4token-wrapperTOP"
-                    style={{
-                      background: "none",
-                      boxShadow: "none",
-                      padding: 0,
-                    }}
-                  >
-                    {/*<div className="p4token-heading">
-                      <p className='h3'><b>Tokenomics</b></p>
-                   </div>*/}
-                    <div className="col-lg-12 offset-lg-2 ml-0 p-0 d-flex">
-                      <div
-                        className="p4token-content-one col-lg-8 p4token-wrapper"
-                        style={{ display: "grid", padding: 25 }}
-                      >
+              <div className="col-lg-12 offset-lg-1">
+                <div
+                  className="p4dyp-wrapper d-grid d-md-flex"
+                  style={{
+                    background: "none",
+                    boxShadow: "none",
+                    padding: 0,
+                  }}
+                >
+                  <div className="col-lg-8 ml-0 p-0 d-grid py-5">
+                    <div className="d-md-flex col-lg-12 p-0"
+                      style={{ gap: 20 }}>
+                      <div className="p4token-content-one p4dyp-wrapper col-lg-7 d-block align-items-center align-self-center m-0">
                         <p>DYP Contract Address: </p>
                         <p className="p4token-content-strong">
                           <span>
@@ -58,61 +56,44 @@ export default class Tokenomics extends React.Component {
                           </span>
                         </p>
                       </div>
-                      <div className="supplyCirculated col-lg-4 p-0">
-                        <p style={{ fontSize: 12 }}>
+                      <div className="supplyCirculated m-0 col-lg-5 py-3 d-grid mt-3 mt-md-0">
+                        <p style={{ fontSize: 14 }}>
                           Initial Circulating Supply:{" "}
                         </p>
-                        <p className="p4token-content-strong">
+                        <h3 className="p4token-content-strong">
                           <span>
                             {getFormattedNumber(this.state.circSupply, 2)} DYP
                           </span>
-                        </p>
+                        </h3>
                       </div>
                     </div>
-                    <div className="p4token-content">
-                      <div
-                        id="crypto-widget-CoinList"
-                        data-design="classic"
-                        data-coins="defi-yield-protocol"
-                      ></div>
+                    <div className="p4token-content p4dyp-wrapper col-lg-12 px-3 py-3 mt-3 d-grid d-md-flex align-self-center">
+                      <div className="col-md-7 m-0 p-0">
+                        <img src={lineGraph} width={'100%'} alt="line-graph" />
+                      </div>
+                      <div className=" d-none d-md-flex mx-4 p-0" style={{ borderRight: '1px solid #ddd' }}>
+                      </div>
+                      <div className="col-md-4 m-0 p-0 justify-content-start align-self-center pt-4">
+                        <div className="d-flex justify-content-start align-self-center">
+                          <h5>
+                            DeFi Yield Protocol
+                          </h5>
+                          &nbsp;&nbsp;
+                          <span>DYP</span>
+                        </div>
+                        <div className="d-flex justify-content-start">
+                          <h1>$0.0000</h1>
+                          &nbsp;&nbsp;
+                          <span className="align-self-center">
+                            +1.76%
+                          </span>
+                        </div>
+                      </div>
                     </div>
-                    
                   </div>
-                  <div className="p4token-sap">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-10 offset-lg-1">
-                <div className="p4token-sap-wrapper">
-                  <div className="p4token-sap-cotnent">
-                    <ul>
-                      <li>
-                        <span></span>Community
-                      </li>
-                      <li>
-                        <span></span>Team
-                      </li>
-                      <li>
-                        <span></span>Investors
-                      </li>
-                    </ul>
+                  <div className="col-lg-4 d-flex justify-content-center align-self-center">
+                    <img src={graphic} />
                   </div>
-                  <div className="p4token-sap-img">
-                    <img
-                      id="whitelink"
-                      src="/img/tsap.svg"
-                      alt="images not found"
-                    />
-                    <img
-                      id="darklink"
-                      src="/img/dstp.svg"
-                      alt="images not found"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
                 </div>
               </div>
             </div>
@@ -122,105 +103,131 @@ export default class Tokenomics extends React.Component {
         <div className="p4dyp">
           <div className="container">
             <div className="row">
-              <div className="col-lg-10 offset-lg-1">
-                <div className="p4dyp-wrapper">
-                  <div className="p4dyp-heading">
-                    <h5>DYP Allocation</h5>
-                    <p>
-                      30,000,000 DYP have been minted at genesis and will become
-                      accessible over the course of 2 years. <br />
-                      The 2-year allocation is as follows:
-                    </p>
-                  </div>
-                  <div className="p4dyp-content">
-                    <div className="p4dyp-content-one">
-                      <div className="p4dyp-content-one-left">
-                        <p>Community</p>
-                      </div>
-                      <div className="p4dyp-content-one-left">
-                        <h4>12,000,000 DYP</h4>
-                        <p>
-                          Distributed for pool rewards over the next 12 months
-                        </p>
-                      </div>
-                      <div className="p4dyp-content-one-left">
-                        <h4>4,500,000 DYP</h4>
-                        <p>Distributed to Binance Smart Chain</p>
-                      </div>
-                      <div className="p4dyp-content-one-left">
-                        <h4>2,090,000 DYP</h4>
-                        <p>Distributed to Avalanche Network</p>
-                      </div>
-                      <div className="p4dyp-content-one-left">
-                        <h4>1,214,111 DYP</h4>
-                        <p>
-                          Distributed for providing liquidity to Centralized
-                          Exchanges and Marketing Purposes
-                        </p>
-                      </div>
-                      <div className="p4dyp-content-one-left">
-                        <h4>2,584,689 DYP</h4>
-                        <p>
-                          Reserved for other pools or chains - Locked on smart
-                          contract
-                        </p>
-                      </div>
-                      <div
-                        className="p4dyp-content-one-left"
-                        style={{ borderBottom: 0 }}
-                      >
-                        <h4>200,000 DYP</h4>
-                        <p>
-                          Locked for one year to Uniswap liquidity on token
-                          launch
-                        </p>
-                      </div>
+              <div className="col-lg-10 offset-lg-1 justify-content-start">
+                <div className="supplyCirculated">
+                  30,000,000 DYP have been minted at genesis and will become
+                  accessible over the course of 2 years. &nbsp;
+                  <span>The 2-year allocation is as follows:</span>
+                </div>
+                <div className="p4dyp-content-one-left justify-content-start m-auto w-100">
+                  <h4>Community</h4>
+                </div>
+                <div
+                  className="d-grid d-md-flex justify-content-start m-auto"
+                  style={{ gap: 40 }}
+                >
+                  <div className="p4dyp-wrapper">
+                    <div className="p4dyp-content-one-left">
+                      <p>
+                        Distributed for pool rewards over the next 12 months
+                      </p>
+                      <h3>
+                        12,000,000.00 <span>DYP</span>
+                      </h3>
                     </div>
                   </div>
-                  <div className="p4dyp-content">
-                    <div className="p4dyp-content-one">
-                      <div className="p4dyp-content-one-left">
-                        <p>Team</p>
-                      </div>
-                      <div
-                        className="p4dyp-content-one-left"
-                        style={{ borderBottom: 0 }}
-                      >
-                        <h4>2,411,200 DYP</h4>
-                        <p>Vested for 24 months, released monthly</p>
-                      </div>
+                  <div className="p4dyp-wrapper">
+                    <div className="p4dyp-content-one-left">
+                      <p>Distributed to Binance Smart Chain</p>
+                      <h3>4,500,000.00 DYP</h3>
                     </div>
                   </div>
-                  <div className="p4dyp-content">
-                    <div className="p4dyp-content-one">
-                      <div className="p4dyp-content-one-left">
-                        <p>Investors</p>
-                      </div>
-                      <div className="p4dyp-content-one-left">
-                        <h4>651,531 DYP</h4>
-                        <p>Public Sale</p>
-                      </div>
-                      <div
-                        className="p4dyp-content-one-left"
-                        style={{ borderBottom: 0 }}
-                      >
-                        <h4>4,348,469 DYP</h4>
-                        <p style={{ wordWrap: "break-word" }}>
-                          BURNED & REMOVED from the total supply TH: <br />
-                          <a
-                            href="https://etherscan.io/tx/0x69d1faef4d41752c200dfe68b103c2823f94f225c502b455321bae0d94ebf8a5"
-                            target="_blank"
-                          >
-                            0x69d1faef4d41752c200dfe68b103c2823f94f225c502b455321bae0d94ebf8a5
-                          </a>
-                        </p>
-                      </div>
+                  <div className="p4dyp-wrapper">
+                    <div className="p4dyp-content-one-left">
+                      <p>Distributed to Avalanche Network</p>
+                      <h3>2,090,000.00 DYP</h3>
                     </div>
                   </div>
-                  <div className="p4dyp-can">
-                    <p id="noadd">
-                      <strong>No Additional Tokens Can Be Minted!</strong>
-                    </p>
+                </div>
+              </div>
+              <div className="col-md-12 col-lg-11 offset-lg-1 justify-content-start">
+                <div className="d-grid d-md-flex m-auto" style={{ gap: 40 }}>
+                  <div className="p4dyp-wrapper">
+                    <div className="p4dyp-content-one-left">
+                      <p>
+                        Distributed for providing liquidity to CEX and Marketing
+                      </p>
+                      <h3>1,214,111.00 DYP</h3>
+                    </div>
+                  </div>
+                  <div className="p4dyp-wrapper">
+                    <div className="p4dyp-content-one-left">
+                      <p>
+                        Reserved for other pools or chains - Locked on smart
+                        contract
+                      </p>
+                      <h3>2,584,689.00 DYP</h3>
+                    </div>
+                  </div>
+                  <div className="p4dyp-wrapper">
+                    <div className="p4dyp-content-one-left">
+                      <p>
+                        Locked for one year to Uniswap liquidity on token launch
+                      </p>
+                      <h3>200,000.00 DYP</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-10 offset-lg-1 justify-content-start">
+                <div className="p4dyp-content-one-left justify-content-start m-auto w-100">
+                  <h4>Investors</h4>
+                </div>
+                <div
+                  className="d-grid d-md-flex justify-content-start m-auto"
+                  style={{ gap: 40 }}
+                >
+                  <div className="p4dyp-wrapper">
+                    <div className="p4dyp-content-one-left">
+                      <p>Public Sale</p>
+                      <h3>651,531.00 DYP</h3>
+                    </div>
+                  </div>
+                  <div
+                    className="p4dyp-wrapper d-grid d-md-flex align-content-center"
+                    style={{ gap: 20 }}
+                  >
+                    <div className="p4dyp-content-one-left">
+                      <p style={{ wordWrap: "break-word" }}>
+                        Burned and removed from the total supply
+                      </p>
+                      <h3>4,348,469.00 DYP</h3>
+                    </div>
+                    <div className="p4dyp-content-red-button">
+                      <a
+                        href="https://etherscan.io/tx/0x69d1faef4d41752c200dfe68b103c2823f94f225c502b455321bae0d94ebf8a5"
+                        target="_blank"
+                        without
+                        rel="noreferrer"
+                      >
+                        View transaction &nbsp;{" "}
+                        <img src={rightArrow} alt="arrow" />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-10 offset-lg-1 justify-content-start">
+                <div className="p4dyp-content-one-left justify-content-start m-auto w-100">
+                  <h4>Team</h4>
+                </div>
+                <div
+                  className="d-grid d-md-flex justify-content-start m-auto"
+                  style={{ gap: 40 }}
+                >
+                  <div
+                    className="p4dyp-wrapper d-grid d-md-flex align-content-center"
+                    style={{ gap: 20 }}
+                  >
+                    <div className="p4dyp-content-one-left">
+                      <p>Vested for 24 months, released monthly</p>
+                      <h3 className="pr-4">2,411,200.00 DYP</h3>
+                    </div>
+                  </div>
+                </div>
+                <div className="d-grid d-md-flex justify-content-start m-auto">
+                  <div className="supplyCirculated justify-content-center d-flex">
+                    No additional tokens can be minted
                   </div>
                 </div>
               </div>
