ccad1826b6384979f18cf1a53394c69d8bc00184
LorenaL579 <lorena.lici579@gmail.com>
2022-04-13 16:44:21 +0200

    updated text in homepage

--- src/components/Home/MainHero/MainHero.js
+++ src/components/Home/MainHero/MainHero.js
@@ -7,79 +7,120 @@ import AuditedByItem from "../../../assets/General/AuditedByItem";
 import ChevronArrowSvg from "../../../assets/General/ChevronArrowSvg/ChevronArrowSvg";
 import { Carousel } from "react-responsive-carousel";
 
+const MainHero = ({ audited, eth, bnb, avax, liquidity, tvl, users }) => {
+  return (
+    <div className="main-hero">
+      <div className="circle-decoration"></div>
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-md-5">
+            <div
+              id="carouselHero"
+              className="carousel slide"
+              data-ride="carousel"
+            >
+              <ol className="carousel-indicators">
+                <li
+                  data-target="#carouselHero"
+                  data-slide-to="0"
+                  className="active"
+                ></li>
+                <li data-target="#carouselHero" data-slide-to="1"></li>
+                <li data-target="#carouselHero" data-slide-to="2"></li>
+              </ol>
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <TitleWithParagraph isMain>
+                    <h1>
+                      Earn{" "}
+                      <mark>
+                        {localStorage.getItem("highapy")} % <br />
+                        APR{" "}
+                      </mark>{" "}
+                      <br />
+                      on Farming
+                    </h1>
+                    <p>
+                    No impermanent loss! Deposit DYP tokens into the Staking contract to start earning. Choose from two staking options, with rewards from 75% APY to 200% APY, depending on the lock time.
+                    </p>
+                  </TitleWithParagraph>
+                </div>
+                <div className="carousel-item">
+                  <TitleWithParagraph isMain>
+                    <h1>
+                  =
+                    Maximize Yield
 
-const MainHero = ({audited, eth, bnb, avax, liquidity, tvl, users}) => {
-    return (
-        <div className="main-hero">
-            <div className="circle-decoration"></div>
-            <div className="container">
-                <div className="row align-items-center">
-                    <div className="col-md-5">
-                        <div id="carouselHero" className="carousel slide" data-ride="carousel">
-                            <ol className="carousel-indicators">
-                                <li data-target="#carouselHero" data-slide-to="0" className="active"></li>
-                                <li data-target="#carouselHero" data-slide-to="1"></li>
-                                <li data-target="#carouselHero" data-slide-to="2"></li>
-                            </ol>
-                            <div className="carousel-inner">
-                                <div className='carousel-item active'>
-                                    <TitleWithParagraph isMain >
-                                        <h1>
-                                            Earn <mark >{localStorage.getItem('highapy')} % <br/>APR </mark> <br />
-                                            on Farming
-                                        </h1>
-                                        <p >
-                                            <mark >DeFi Yield Protocol</mark> Lorem ipsum dolor sit amert
-                                            <mark >anti-manipulation</mark> Lorem ipsum dolor sit amert Lorem ipsum dolor sit amert
-                                        </p>
-                                    </TitleWithParagraph>
-                                </div>
-                                <div className='carousel-item'>
-                                    <TitleWithParagraph isMain >
-                                        <h1>
-                                            Earn <mark >{localStorage.getItem('highapy')} % <br/> APR  </mark> <br />
-                                            on Farming
-                                        </h1>
-                                        <p >
-                                            <mark >DeFi Yield Protocol</mark> Lorem ipsum dolor sit amert
-                                            <mark >anti-manipulation</mark> Lorem ipsum dolor sit amert Lorem ipsum dolor sit amert
-                                        </p>
-                                    </TitleWithParagraph>
+                      <mark>
+                      Farming Rewards!
+                      </mark>{" "}
+                     
+                    </h1>
+                    <p>
+                      No impermanent loss! Deposit DYP tokens into the Staking
+                      contract to start earning. Choose from two staking
+                      options, with rewards from 75% APY to 200% APY, depending
+                      on the lock time.
+                    </p>
+                  </TitleWithParagraph>
+                </div>
+                <div className="carousel-item">
+                  <TitleWithParagraph isMain>
+                    <h1>
+                    Maximize Yield
 
-                                </div>
-                                <div className='carousel-item'>
-                                    <TitleWithParagraph isMain >
-                                        <h1>
-                                            Earn <mark >{localStorage.getItem('highapy')} % <br/> APR  </mark> <br />
-                                            on Farming
-                                        </h1>
-                                        <p >
-                                            <mark >DeFi Yield Protocol</mark> Lorem ipsum dolor sit amert
-                                            <mark >anti-manipulation</mark> Lorem ipsum dolor sit amert Lorem ipsum dolor sit amert
-                                        </p>
-                                    </TitleWithParagraph>
-                                </div>
-                            </div>
-                        </div>
+                      <mark>
+                      Farming Rewards!
+                      </mark>{" "}
+                     
+                    </h1>
+                    <p>It’s minting time!
 
+                      Our brand-new NFTs, the Cats And Watches society have
+                      officially started minting! With over 190 different traits
+                      to choose from, these randomly-generated cats are ready to
+                      become yours.
+                    </p>
+                  </TitleWithParagraph>
+                </div>
+              </div>
+            </div>
 
-                        <Button text="Enter now" icon={<ChevronArrowSvg />} action={() => console.log("clicked")} />
-
-                    </div>
-                    {/* to be checked if text on blue cards is static */}
-                    <div className="col-md-7 main-hero-graphic-wrapper">
-                        <img className='img-fluid' src={require("../../../assets/Home/banner_1.png")} />
-                        <div className='text-wrapper'>
-                            <p>{eth} <small>ETH</small>, {bnb} <small>BNB</small></p>
-                            <p>{avax}<small> AVAX worth:</small></p>
-                            <p> $ {liquidity}</p>
-                            <p><small>paid to Liquidity providers!</small></p>
-                        </div>
-                        <div className='text-wrapper-secondary'>
-                            <p><small>TVL </small>$ {tvl}</p>
-                            <p><small>Total users </small>{users}</p>
-                        </div>
-                        {/* <MainHeroBanner
+            <Button
+              text="Enter now"
+              icon={<ChevronArrowSvg />}
+              action={() => console.log("clicked")}
+            />
+          </div>
+          {/* to be checked if text on blue cards is static */}
+          <div className="col-md-7 main-hero-graphic-wrapper">
+            <img
+              className="img-fluid"
+              src={require("../../../assets/Home/banner_1.png")}
+            />
+            <div className="text-wrapper">
+              <p>
+                {eth} <small>ETH</small>, {bnb} <small>BNB</small>
+              </p>
+              <p>
+                {avax}
+                <small> AVAX worth:</small>
+              </p>
+              <p> $ {liquidity}</p>
+              <p>
+                <small>paid to Liquidity providers!</small>
+              </p>
+            </div>
+            <div className="text-wrapper-secondary">
+              <p>
+                <small>TVL </small>$ {tvl}
+              </p>
+              <p>
+                <small>Total users </small>
+                {users}
+              </p>
+            </div>
+            {/* <MainHeroBanner
                             eth={eth}
                             bnb={bnb}
                             avax={avax}
@@ -87,33 +128,32 @@ const MainHero = ({audited, eth, bnb, avax, liquidity, tvl, users}) => {
                             tvl={tvl}
                             users={users}
                         /> */}
-                    </div>
-                </div>
-                <div className="row ">
-                    <div className="col">
-                        <p className='audited-by-title'>
-                            Audited by
-                        </p>
-                        <div className="audited-by-section">
-                            {audited.length > 0 && audited.map((item, id) => (
-                                <AuditedByItem key={id} imgName={item.img} text={item.name} />)
-                            )}
-                        </div>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-    )
-}
+        <div className="row ">
+          <div className="col">
+            <p className="audited-by-title">Audited by</p>
+            <div className="audited-by-section">
+              {audited.length > 0 &&
+                audited.map((item, id) => (
+                  <AuditedByItem key={id} imgName={item.img} text={item.name} />
+                ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 MainHero.propTypes = {
-    audited: PropTypes.array,
-    eth: PropTypes.string,
-    bnb: PropTypes.string,
-    avax: PropTypes.string,
-    liquidity: PropTypes.string,
-    tvl: PropTypes.string,
-    users: PropTypes.string,
-}
+  audited: PropTypes.array,
+  eth: PropTypes.string,
+  bnb: PropTypes.string,
+  avax: PropTypes.string,
+  liquidity: PropTypes.string,
+  tvl: PropTypes.string,
+  users: PropTypes.string,
+};
 
-export default MainHero
+export default MainHero;
