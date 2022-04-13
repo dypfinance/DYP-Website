26b76d27a83c8a0b0211e67dfabae4a4a97e3315
doyledyp <doyledev@protonmail.com>
2022-04-13 15:09:18 +0100

    Update Card Modal CAWS

--- src/components/nft/Caws.js
+++ src/components/nft/Caws.js
@@ -17,6 +17,8 @@ const Caws = () => {
     const [connectedWallet, setConnectedWallet] = useState(false)
     const [myNFTs, setMyNFTs] = useState([])
 
+    const link = 'https://dyp.finance/mint'
+
     function range(start, end, step = 1) {
         const len = Math.floor((end - start) / step) + 1
         return Array(len).fill().map((_, idx) => start + (idx * step))
@@ -104,6 +106,7 @@ const Caws = () => {
                 modalId='newNft'
                 nftItem={openedNft}
                 visible={openedNft ? true : false}
+                link={link}
                 onShareClick={onShareClick}
             />
             </div>
