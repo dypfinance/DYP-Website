6de2bfd2fd6c4815e3aecf8a1d8d33a90c67f2fc
doyledyp <doyledev@protonmail.com>
2022-04-14 19:08:20 +0100

    Update CAWS & Mint Style

--- src/components/CtaButton/CtaButton.js
+++ src/components/CtaButton/CtaButton.js
@@ -8,13 +8,13 @@ const  CtaButton = () => {
     <div className="col-lg-2" style={{marginTop: '190px'}}>
       <div>
         <h3 className="startTitle">Start</h3>
-        <div className="d-flex flex-column align-items-center">
-         <img src={TripleArrow} alt="arrow" />
-         
-     <img src={CtaBtn} className='ctabtn'/> 
-       
-        <NavLink class="mintBtn mt-4" to='/mint'><span>GO MINT</span></NavLink>
-        </div>
+          <NavLink to='/mint'>
+            <div className="d-flex flex-column align-items-center">
+                <img src={TripleArrow} alt="arrow" />
+                <img src={CtaBtn} className='ctabtn'/>
+                <NavLink class="mintBtn mt-4" to='/mint'><span>GO MINT</span></NavLink>
+            </div>
+          </NavLink>
       </div>
     </div>
   );
