b5ac12a7cf05adb51aa68c5cf46718d69901dbb0
ortensa <ortensal77@gmail.com>
2022-05-17 20:37:43 +0200

    brigde layout fix

--- src/components/bscbridge.js
+++ src/components/bscbridge.js
@@ -6,12 +6,14 @@ import DoubleArrow from "../assets/images/doubleArrows.svg";
 import DypLogo from "../assets/images/dypLogo.svg";
 import IDypLogo from '../assets/images/idypLogo.svg'
 import LinkLogo from "../assets/images/linkIcon.svg";
+import rightArrow from "../../src/assets/svg/white-arrow.svg";
+import ChevronArrowSvg from '../../src/assets/General/ChevronArrowSvg/ChevronArrowSvg'
 
 export default class Bscbridge extends React.Component {
   render() {
     return (
       <>
-        <div className="earn-hero-area App-container"  style={{background: 'none'}}>
+        <div className="earn-hero-area App-container" style={{ background: 'none' }}>
           <div className="container">
             <div className="earn-hero-wrapper">
               <div className="row" style={{ justifyContent: "center" }}>
@@ -31,8 +33,8 @@ export default class Bscbridge extends React.Component {
                           <span>
                             <img src={DoubleArrow} alt="arrow" />
                           </span>
-                          <span>
-                            <img src={BscLogo} alt="bsc" /> BSC
+                          <span className="chainText">
+                            BSC <img src={BscLogo} alt="bsc" />
                           </span>
                         </div>
                       </div>
@@ -45,42 +47,47 @@ export default class Bscbridge extends React.Component {
                         }}
                       >
                         <img src={DypLogo} alt="dyp" height={35} />
-                        <b>DYP BSC Bridge</b>
+                        <span className="brige-box-title">DYP BSC Bridge</span>
                       </h3>
                       <p>
                         The Bridge will help you swap DYP tokens between
                         Ethereum Network and Binance Smart Chain Network.
                       </p>
                       <div className="bottomWrapper">
-                        <p className="mb-0">
-                          <img src={LinkLogo} alt="" className="mr-2" />
-                          For more information you can visit our{" "}
-                          <a target="_blank" href="https://dypfinance.medium.com/defi-yield-protocol-bridge-tutorial-9dad5226537f">
-                            <span style={{ color: "#1D91D0" }}>
-                            medium bridge tutorial
-                            </span>
-                          </a>{" "}
-                          article
+                        <p className="mb-0 d-flex">
+                          <div>
+                            <img src={LinkLogo} alt="" className="mr-2" />
+                          </div>
+                          <div>For more information you can visit our&nbsp;
+                            <a target="_blank" href="https://dypfinance.medium.com/defi-yield-protocol-bridge-tutorial-9dad5226537f">
+                              <u style={{ color: "#1D91D0" }}>medium bridge tutorial</u>
+                            </a>&nbsp;
+                            article
+                          </div>
                         </p>
                       </div>
                       <div className="buttonWrapper">
-                        <div className="earn-hero-btns-bridge m-0" style={{maxWidth: 274, width: '100%'}}>
+                        <div className="earn-hero-btns-bridge m-0" style={{ maxWidth: 274, width: '100%' }}>
                           <a
                             target="_blank"
                             className="m-0"
-                            style={{borderRadius: 6,maxWidth: 274, width: '100%'}}
+                            style={{ borderRadius: 6, maxWidth: 274, width: '100%' }}
                             href="https://bridge.dyp.finance"
                           >
-                            Go to bridge platform
+                            Go to bridge platform &nbsp;&nbsp;&nbsp;
+                            <img src={rightArrow} />
                           </a>
                         </div>
-                        <div className="earn-hero-btns-white m-0" style={{maxWidth: 274, width: '100%'}}>
+                        <div className="earn-hero-btns-white m-0" style={{ maxWidth: 274, width: '100%' }}>
                           <a
                             target="_blank"
                             href="https://www.youtube.com/watch?v=SK-9PqZJM-I"
-                            style={{borderRadius: 6,maxWidth: 274, width: '100%'}}
+                            style={{ borderRadius: 6, maxWidth: 274, width: '100%' }}
                           >
-                            Video Tutorial
+                            Video Tutorial &nbsp;&nbsp;&nbsp;
+                            <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M1 11L6 6L1 1" stroke="#EA1C1D" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
                           </a>
                         </div>
                       </div>
@@ -103,8 +110,8 @@ export default class Bscbridge extends React.Component {
                           <span>
                             <img src={DoubleArrow} alt="arrow" />
                           </span>
-                          <span>
-                            <img src={BscLogo} alt="bsc" /> BSC
+                          <span className="chainText">
+                            BSC <img src={BscLogo} alt="bsc" />
                           </span>
                         </div>
                       </div>
@@ -117,40 +124,47 @@ export default class Bscbridge extends React.Component {
                         }}
                       >
                         <img src={IDypLogo} alt="dyp" height={35} />
-                        <b>iDYP BSC Bridge</b>
+                        <span className="brige-box-title">iDYP BSC Bridge</span>
                       </h3>
                       <p>
                         The Bridge will help you swap iDYP tokens between
                         Ethereum Network and Binance Smart Chain Network.
                       </p>
                       <div className="bottomWrapper">
-                        <p className="mb-0">
-                          <img src={LinkLogo} alt="" className="mr-2" />
-                          For more information you can visit our{" "}
-                          <a target="_blank" href="https://dypfinance.medium.com/defi-yield-protocol-bridge-tutorial-9dad5226537f">
-                            <span style={{ color: "#1D91D0" }}> medium bridge tutorial</span>
-                          </a>{" "}
-                          article
+                        <p className="mb-0 d-flex">
+                          <div>
+                            <img src={LinkLogo} alt="" className="mr-2" />
+                          </div>
+                          <div>For more information you can visit our&nbsp;
+                            <a target="_blank" href="https://dypfinance.medium.com/defi-yield-protocol-bridge-tutorial-9dad5226537f">
+                              <u style={{ color: "#1D91D0" }}>medium bridge tutorial</u>
+                            </a>&nbsp;
+                            article
+                          </div>
                         </p>
                       </div>
                       <div className="buttonWrapper">
-                        <div className="earn-hero-btns-bridge m-0" style={{maxWidth: 274, width: '100%'}}>
+                        <div className="earn-hero-btns-bridge m-0" style={{ maxWidth: 274, width: '100%' }}>
                           <a
                             target="_blank"
                             className="m-0"
                             href="https://ibridge.dyp.finance"
-                            style={{borderRadius: 6,maxWidth: 274, width: '100%'}}
+                            style={{ borderRadius: 6, maxWidth: 274, width: '100%' }}
                           >
-                            Go to bridge platform
+                            Go to bridge platform &nbsp;&nbsp;&nbsp;
+                            <img src={rightArrow} />
                           </a>
                         </div>
-                        <div className="earn-hero-btns-white m-0" style={{maxWidth: 274, width: '100%'}}>
+                        <div className="earn-hero-btns-white m-0" style={{ maxWidth: 274, width: '100%' }}>
                           <a
                             target="_blank"
                             href="https://www.youtube.com/watch?v=SK-9PqZJM-I"
-                            style={{borderRadius: 6,maxWidth: 274, width: '100%'}}
+                            style={{ borderRadius: 6, maxWidth: 274, width: '100%' }}
                           >
-                            Video Tutorial
+                            Video Tutorial &nbsp;&nbsp;&nbsp;
+                            <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M1 11L6 6L1 1" stroke="#EA1C1D" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
                           </a>
                         </div>
                       </div>
