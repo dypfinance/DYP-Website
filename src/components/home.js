d84fd8c3051b152f81c9fb160b2ca464c7b92501
doyledyp <doyledev@protonmail.com>
2022-05-23 09:55:04 +0100

    Update Home Text

--- src/components/home.js
+++ src/components/home.js
@@ -1668,7 +1668,7 @@ export default class Home extends React.Component {
         const auditedByArray = [
             {
                 img: "audited-by-item-1",
-                name: "Peck Shield",
+                name: "PeckShield",
             },
             {
                 img: "audited-by-item-2",
@@ -1676,7 +1676,7 @@ export default class Home extends React.Component {
             },
             {
                 img: "audited-by-item-3",
-                name: "Certik  Foundation",
+                name: "CertiK Security",
             },
         ];
 
