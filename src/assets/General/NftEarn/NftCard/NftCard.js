92d64ec323cebd8c1f7923455f2f2b33cc46e8e1
LorenaL579 <lorena.lici579@gmail.com>
2022-08-23 13:36:00 +0200

    fixed bug with stake nft card

--- src/assets/General/NftEarn/NftCard/NftCard.js
+++ src/assets/General/NftEarn/NftCard/NftCard.js
@@ -7,73 +7,24 @@ import EthLogo from "../../../../assets/General/eth-create-nft.png";
 
 const NftCard = () => {
   const [mystakes, setMystakes] = useState([]);
-  const [connectedWallet, setConnectedWallet] = useState(false);
-  const [isConnectedWallet, setIsConnectedWallet] = useState(false);
-  const [countDownLeft, setCountDownLeft] = useState(59000);
-
-  const handleConnectWallet = async () => {
-    try {
-      let isConnected = await window.connectWallet();
-
-      if (isConnected) {
-        setIsConnectedWallet(true);
-        let coinbase = await window.getCoinbase();
-        // Set Coinbase State
-        setConnectedWallet(coinbase);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
 
   const myStakes = async () => {
-    let staking_contract = await window.getContract("NFT");
+    let staking_contract = await new window.infuraWeb3.eth.Contract(
+      window.NFT_ABI,
+      window.config.nft_address,
+      {from: undefined}
+  );
     await staking_contract.methods
-    .balanceOf('0xEe425BbbEC5e9Bf4a59a1c19eFff522AD8b7A47A')
+    .balanceOf(window.config.nftstaking_address)
     .call()
     .then((data)=>{setMystakes(data)});
+    
   };
 
-  const calculateCountdown = async () => {
-    const address = await window.getCoinbase();
-
-    let staking_contract = await window.getContract("NFTSTAKING");
-    let finalDay = await staking_contract.methods
-      .stakingTime(address)
-      .call()
-      .then((data) => {
-        return data;
-      })
-      .catch((err) => {
-        // window.alertify.error(err?.message);
-      });
-
-    let lockup_time = await staking_contract.methods
-      .LOCKUP_TIME()
-      .call()
-      .then((data) => {
-        return data;
-      })
-      .catch((err) => {
-        // window.alertify.error(err?.message);
-      });
-
-    finalDay = parseInt(finalDay) + parseInt(lockup_time);
-
-    setCountDownLeft(parseInt(finalDay * 1000) - Date.now());
-  };
 
   useEffect(() => {
-    handleConnectWallet().then();
-    calculateCountdown().then();
-  }, []);
-
-  useEffect(() => {
-    if (connectedWallet) {
       myStakes().then();
-    }
-  }, [connectedWallet]);
+  }, []);
 
   return (
     <div className="subasset-card-with-button col-md-3">
