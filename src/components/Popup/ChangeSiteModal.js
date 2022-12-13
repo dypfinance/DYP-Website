6a446b32f5aba701a39e948d94d18b8e3ff70c47
doyledyp <doyledev@protonmail.com>
2022-12-13 14:12:20 +0200

    Remove Redirect

--- src/components/Popup/ChangeSiteModal.js
+++ src/components/Popup/ChangeSiteModal.js
@@ -16,31 +16,31 @@ const ChangeSiteModal = ({onModalClose, modalState}) => {
     useEffect(() => {
      
     if (modalState === true) {
-        html.classList.add('hidescroll')
+        // html.classList.add('hidescroll')
         siteModal?.classList.add('showscroll');
       } else {
         // Enable scroll
         html.classList.remove('hidescroll')
       }
-      if(modalState === true){
-        setTimeout(() => {
-            window.location.replace('https://dypius.com/')
-          }, 5000);
-      }
+      // if(modalState === true){
+      //   setTimeout(() => {
+      //       window.location.replace('https://dypius.com/')
+      //     }, 5000);
+      // }
     }, [modalState])
     
     
 
   return (
-    <a href='https://dypius.com/' className={`change-site-modal ${modalState && 'change-modal-open'} d-flex flex-column align-items-center justify-content-center px-5 pb-5 gap-3`} id="changeModal">
+    <div className={`change-site-modal ${modalState && 'change-modal-open'} d-flex flex-column align-items-center justify-content-center px-5 pb-5 gap-3`} id="changeModal">
         <img src={xMark} className="x-mark" alt="" onClick={onModalClose} />
         <img src={siteChange} alt="" className='modal-image' />
         <img src={techDypius} alt="" className='modal-image' />
         <img src={defiChange} alt="" className='modal-image' />
         <a href="https://dypius.com/">
-        <img src={continueButton} alt="" width={200} />
+            <img src={continueButton} alt="" width={200} />
         </a>
-    </a>
+    </div>
   )
 }
 
